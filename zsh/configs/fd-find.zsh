7f35ebe2756af93258d671ff40a8b05feb7bfbcf
Hannes Van de Vel <h@nnes.be>
2019-08-16 12:50:02 +0200

    Added fd-find installer

---
+++ zsh/configs/fd-find.zsh
@@ -0,0 +1,18 @@
+####################################################################
+# Install fd - A simple, fast and user-friendly alternative to 'find'
+####################################################################
+if ! (( $+commands[fdfind] )); then
+  echo "Installing fd"
+
+  # OS specific installation steps
+  case "$DISTRO" in
+    debian|ubuntu|elementary|mint)
+    sudo apt install fd-find >/dev/null 2>&1
+    echo " OK"
+    ;;
+    *)
+    echo "No install procedure for your OS available."
+    ;;
+  esac
+fi
+alias fd=fdfind
