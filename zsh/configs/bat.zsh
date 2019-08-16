d123f3d5cd6681c8ef8e8a6f2a94adf3aa371d95
Hannes Van de Vel <h@nnes.be>
2019-08-16 06:23:25 +0200

    Updated bat install

--- zsh/configs/bat.zsh
+++ zsh/configs/bat.zsh
@@ -3,22 +3,19 @@
 ####################################################################
 if ! type "bat" > /dev/null; then
   echo "Installing bat - a 'cat' clone on wings"
+  LATEST_BAT_VERSION=$( curl -s https://api.github.com/repos/sharkdp/bat/releases/latest | grep '"tag_name":' | cut -d '"' -f 4 | sed 's/v//' )
 
   # OS specific installation steps
-  if [[ $DISTRO == 'darwin' ]]; then
-
-  elif [[ $DISTRO == 'centos' || $DISTRO == 'centminmod' ]]; then
-
-  elif [[ $DISTRO == 'ubuntu' || $DISTRO == 'elementary' || "${DISTRO#*debian}" != "$DISTRO" ]]; then
-    LATEST_BAT_VERSION=$( curl -s https://api.github.com/repos/sharkdp/bat/releases/latest | grep '"tag_name":' | cut -d '"' -f 4 | sed 's/v//' )
+  case "$DISTRO" in
+    debian|ubuntu|elementary|mint)
     BAT_FILE_NAME=bat_$LATEST_BAT_VERSION'_amd64.deb'
     wget https://github.com/sharkdp/bat/releases/download/v$LATEST_BAT_VERSION/$BAT_FILE_NAME >/dev/null 2>&1
     sudo dpkg -i $BAT_FILE_NAME >/dev/null 2>&1
     rm -f $BAT_FILE_NAME >/dev/null 2>&1
     echo " OK"
-  elif [[ $DISTRO == 'cygwin' ]]; then
-
-  else
-
-  fi
+    ;;
+    *)
+    echo "No install procedure for your OS available."
+    ;;
+  esac
 fi
