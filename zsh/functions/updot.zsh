385528733dd070c88d050b073fc91fa954d8261f
Hannes Van de Vel <h@nnes.be>
2019-08-16 04:27:06 +0200

    Less verbose output updot function

--- zsh/functions/updot.zsh
+++ zsh/functions/updot.zsh
@@ -3,10 +3,9 @@ function updot () {
   pushd ~/.dotfiles
   git remote set-url origin https://github.com/Connexeon/dotfiles.git
   git pull origin master
-  ./install
-  upgrade_oh_my_zsh
-  antigen-selfupdate
-  update-submodules
-  echo "Relogin or run 'exec zsh' to apply changes"
+  chronic upgrade_oh_my_zsh
+  chronic antigen selfupdate
+  chronic update-submodules
+  chronic ./install && echo "[ OK ] Relogin or run 'exec zsh' to apply changes"
   popd
 }
