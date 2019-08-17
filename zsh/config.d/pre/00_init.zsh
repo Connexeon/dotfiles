541c5fa39a465587a7cf73c97bafb22a08ef849e
Hannes Van de Vel <h@nnes.be>
2019-08-17 13:16:09 +0200

    Update color helper vars to env vars

--- zsh/config.d/pre/00_init.zsh
+++ zsh/config.d/pre/00_init.zsh
@@ -1,9 +1,11 @@
-# Some output definitions
-N=$(tput sgr0)
-BD=$(tput bold)
-GR=$(tput setaf 2; tput bold)
-YE=$(tput setaf 3)
-R=$(tput setaf 1)
-OK="[$GR OK $N]\n"
-FL="[$R FAILED $N]\n"
-WA="[$R WARNING $N]\n"
+# Some basic env variables to bring color
+# & consistent OK / FL / WA indicators.
+
+export  N=$(tput sgr0)
+export BD=$(tput bold)
+export GR=$(tput setaf 2; tput bold)
+export YE=$(tput setaf 3)
+export  R=$(tput setaf 1)
+export OK=" [$GR OK $N]\n"
+export FL=" [$R FAILED $N]\n"
+export WA=" [$R WARNING $N]\n"
