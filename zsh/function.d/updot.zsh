f4b2012a27b24ca01b38ff9f4ae7d93a4f0d4955
Hannes Van de Vel <h@nnes.be>
2019-08-17 05:18:46 +0200

    Reduce output from updot & reinstate history substring, autosuggestions & syntax highlighting

--- zsh/function.d/updot.zsh
+++ zsh/function.d/updot.zsh
@@ -1,11 +1,11 @@
 # Update dotfiles & dependencies
 function updot () {
   pushd ~/.dotfiles
-  git remote set-url origin https://github.com/Connexeon/dotfiles.git >/dev/null 2>&1
-  git pull origin master >/dev/null 2>&1
-  upgrade_oh_my_zsh >/dev/null 2>&1
-  antigen selfupdate >/dev/null 2>&1
-  update-submodules >/dev/null 2>&1
-  ./install && echo "[ OK ] Relogin or run 'exec zsh' to apply changes"
+  git remote set-url origin https://github.com/Connexeon/dotfiles.git >/dev/null 2>&1 ; \
+  git pull origin master >/dev/null 2>&1 ; \
+  upgrade_oh_my_zsh >/dev/null 2>&1 ; \
+  antigen selfupdate >/dev/null 2>&1 ; \
+  update-submodules >/dev/null 2>&1 ; \
+  ./install >/dev/null 2>&1 && printf "$OK Reconnect session or run 'exec zsh' to apply\n" || ( printf " $FL" ; exit -1 )
   popd
 }
