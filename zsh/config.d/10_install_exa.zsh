64f23d85bec1640b6b4830b2742e11a1a2db7967
Hannes Van de Vel <h@nnes.be>
2019-08-17 13:15:14 +0200

    Added exa install options for more distros and binary download as Linux fallback

--- zsh/config.d/10_install_exa.zsh
+++ zsh/config.d/10_install_exa.zsh
@@ -5,17 +5,55 @@ CMD="exa"
 CMDTITLE="A modern version of 'ls'"
 
 if (( ! $+commands[$CMD] )); then
-  echo "Installing $CMD - $CMDTITLE"
+  printf "Installing $CMD - $CMDTITLE"
 
   # OS specific installation steps
   case "$DISTRO" in
-    debian|raspbian|ubuntu|elementary|mint)
-      sudo apt-get -y install exa librust-exa+git\*-dev >/dev/null 2>&1 && printf "$OK" || ( printf " $FL" ; exit -1 )
+    # debian|ubuntu|elementary|mint)
+    #   sudo apt-get -y install exa librust-exa+git\*-dev >/dev/null 2>&1 && printf "$OK" || ( printf "$FL" ; exit -1 )
+    #   ;;
+    osx)
+      brew install exa >/dev/null 2>&1 && printf "$OK" || ( printf "$FL" ; exit -1 )
+      ;;
+    arch)
+      sudo pacman -S exa >/dev/null 2>&1 && printf "$OK" || ( printf "$FL" ; exit -1 )
+      ;;
+    fedora)
+      sudo dnf install exa >/dev/null 2>&1 && printf "$OK" || ( printf "$FL" ; exit -1 )
+      ;;
+    opensuse)
+      sudo zypper install exa >/dev/null 2>&1 && printf "$OK" || ( printf "$FL" ; exit -1 )
+      ;;
+    nixos)
+      nix-env -i exa >/dev/null 2>&1 && printf "$OK" || ( printf "$FL" ; exit -1 )
+      ;;
+    gentoo)
+      emerge sys-apps/exa >/dev/null 2>&1 && printf "$OK" || ( printf "$FL" ; exit -1 )
       ;;
     *)
-      echo "No install procedure for $CMD for your OS/distro available, please install manually."
+      if [[ $OS = "Linux" ]] ; then
+        # Download Linux binary and move it into /usr/local/bin
+        TMP_DIR=$(mktemp -d)
+        EXA_ZIP="exa-linux-x86_64-latest.zip"
+        EXA_REPO="ogham/exa"
+        EXA_ZIP_LATEST_URL=$( curl -s "https://api.github.com/repos/$EXA_REPO/releases/latest" \
+          | jq -r '.assets[] | select(.name | contains("exa-linux-x86_64")) | .browser_download_url' ) ; \
+
+        curl -L $EXA_ZIP_LATEST_URL -o $TMP_DIR/$EXA_ZIP >/dev/null 2>&1 ; \
+        unzip $TMP_DIR/$EXA_ZIP -d $TMP_DIR && sudo cp $TMP_DIR/exa-linux-x86_64 /usr/local/bin/exa >/dev/null 2>&1 && printf "$OK" || ( printf "$FL" ; exit -1 )
+      fi
       ;;
   esac
+
+  # Download/Install shell completion definition
+  COMPDEF="/usr/local/share/zsh/site-functions/_exa"
+  COMPDEFURL="https://raw.githubusercontent.com/ogham/exa/master/contrib/completions.zsh"
+  if [[ -e "$COMPDEF" ]]; then
+    sudo rm -f $COMPDEF
+  fi
+  printf "Installing $CMD shell completion definitions"
+  sudo curl -o $COMPDEF $COMPDEFURL >/dev/null 2>&1 && printf "$OK" || ( printf "$FL" ; exit -1 )
+
 fi
 
 # Load if command exists
