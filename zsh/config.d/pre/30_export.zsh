c6052c2d8ff5c86e86b4a571988961671207cdca
Hannes Van de Vel <h@nnes.be>
2019-08-24 03:21:05 +0200

    Added some exports (omz default config inspired)

--- zsh/config.d/pre/30_export.zsh
+++ zsh/config.d/pre/30_export.zsh
@@ -7,6 +7,23 @@ export CLICOLOR=1
 
 export LESS='--ignore-case --raw-control-chars'
 export PAGER='less'
-export EDITOR='nano'
 
 export ZSH_ALIAS_FINDER_AUTOMATIC=true
+
+export MANPATH="/usr/local/man:$MANPATH"
+
+# You may need to manually set your language environment
+export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='nano'
+else
+  export EDITOR='gedit'
+fi
+
+# Compilation flags
+export ARCHFLAGS="-arch x86_64"
+
+# Uncomment the following line to change how often to auto-update (in days).
+export UPDATE_ZSH_DAYS=2
