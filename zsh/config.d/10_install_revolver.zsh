fdd30a5bd76f27e28035b8269353237aa3d81015
Hannes Van de Vel <h@nnes.be>
2019-08-24 03:21:29 +0200

    Minor update install function revolver

--- zsh/config.d/10_install_revolver.zsh
+++ zsh/config.d/10_install_revolver.zsh
@@ -9,7 +9,7 @@ _install_revolver () {
   chmod u+x $HOME/.zsh/submodules/revolver/revolver ; \
   path+=$HOME/.zsh/submodules/revolver >/dev/null 2>&1 && printf "$OK" || ( printf "$FL" ; exit 1 )
 
-  __curl_compdef $CMD "https://raw.githubusercontent.com/molovo/revolver/master/revolver.zsh-completion"
+  __curl_compdef $1 "https://raw.githubusercontent.com/molovo/revolver/master/revolver.zsh-completion"
 }
 
 # If command does not exist (not yet installed)
