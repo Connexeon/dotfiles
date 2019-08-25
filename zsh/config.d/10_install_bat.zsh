1557019efbb68a2613700f6173160c2f15f6f213
Hannes Van de Vel <h@nnes.be>
2019-08-25 04:19:49 +0200

    Improvements bat installer script

--- zsh/config.d/10_install_bat.zsh
+++ zsh/config.d/10_install_bat.zsh
@@ -6,15 +6,17 @@ CMDTITLE="A 'cat' clone with wings"
 
 _install_bat () {
 
-  LATEST_BAT_VERSION=$( curl -s https://api.github.com/repos/sharkdp/bat/releases/latest | grep '"tag_name":' | cut -d '"' -f 4 | sed 's/v//' )
-
+  BAT_REPO='sharkdp/bat'
+  BAT_LATEST_VERSION=$( curl -s https://api.github.com/repos/$BAT_REPO/releases/latest | grep '"tag_name":' | cut -d '"' -f 4 | sed 's/v//' )
+  BAT_DEB_FILE='bat_'$BAT_LATEST_VERSION'_'$ARCH2'.deb'
+  BAT_DEB_URL='https://github.com/'$BAT_REPO'/releases/download/v'$BAT_LATEST_VERSION'/'$BAT_DEB_FILE
+  TMP_DIR=$(mktemp -d)
   # OS specific installation steps
   case "$DISTRO" in
     debian|raspbian|ubuntu|elementary|mint)
-      BAT_FILE_NAME=bat_$LATEST_BAT_VERSION'_'$ARCH2'.deb'
-      wget https://github.com/sharkdp/bat/releases/download/v$LATEST_BAT_VERSION/$BAT_FILE_NAME >/dev/null 2>&1 ; \
-       "dpkg -i $BAT_FILE_NAME" >/dev/null 2>&1 ; \
-      rm -f $BAT_FILE_NAME >/dev/null 2>&1 && printf "$OK" || ( printf "$FL" ; exit 1 )
+      curl -L $BAT_DEB_URL -o $TMP_DIR/$BAT_DEB_FILE >/dev/null 2>&1 ; \
+      superuser_do "dpkg -i $TMP_DIR/$BAT_DEB_FILE" >/dev/null 2>&1 ; \
+      rm -f $TMP_DIR/$BAT_DEB_FILE >/dev/null 2>&1 && printf "$OK" || ( printf "$FL" ; exit 1 )
       ;;
     *)
       echo "# Added by .dotfiles 10_install_bat.zsh\nexport DOTFILES_BAT_DISABLED=1" >> $HOME/.zshrc_local
