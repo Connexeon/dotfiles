b74aa761008753d624989ca32a090b4f13cb16bb
Hannes Van de Vel <h@nnes.be>
2019-12-02 04:13:55 +0100

    Update updot function to stash & stash pop

--- zsh/function.d/updot.zsh
+++ zsh/function.d/updot.zsh
@@ -1,11 +1,13 @@
 # Update dotfiles & dependencies
 function updot () {
   pushd ~/.dotfiles
+  git stash
   git remote set-url origin https://github.com/Connexeon/dotfiles.git >/dev/null 2>&1 ; \
   git pull origin master >/dev/null 2>&1 ; \
   upgrade_oh_my_zsh >/dev/null 2>&1 ; \
   antigen selfupdate >/dev/null 2>&1 ; \
   update-submodules >/dev/null 2>&1 ; \
   ./install >/dev/null 2>&1 && printf "$OK Reconnect session or run 'exec zsh' to apply\n" || ( printf "$FL" ; exit 1 )
+  git stash pop
   popd
 }
